2a08f05bb18364a31f2673eaed13c6676402b96e
Jessie <jessica.v.rid@gmail.com>
2023-01-15 22:36:28 +0300

    Example005 added

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Enter your name");
+string username = Console.ReadLine();
+if (username == "German")
+{
+    System.Console.WriteLine("VA!");
+}
+else
+{
+    System.Console.Write("Hello, ");
+    System.Console.WriteLine(username);
+}
+
+
