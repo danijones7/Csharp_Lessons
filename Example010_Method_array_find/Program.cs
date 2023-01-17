5d1074adb2ba02b759203228d7053a7cce2b6a15
Jessie <jessica.v.rid@gmail.com>
2023-01-17 16:34:25 +0300

    Ex 010 finding index of array number

---
+++ Example010_Method_array_find/Program.cs
@@ -0,0 +1,14 @@
+﻿//              0  1  2  3  4  5  6  7  8
+int[] array = {11,211,31,41,15,41,17,18,19};
+System.Console.WriteLine("enter number: ");
+int find = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+while(index < array.Length)
+{
+    if (array[index] == find)
+    {
+    System.Console.WriteLine(index);
+    break;
+    }
+index ++;
+}
