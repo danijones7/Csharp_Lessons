7326fc189bacc71eee761abad2861e2be10c6a07
Jessie <jessica.v.rid@gmail.com>
2023-01-31 02:12:04 +0300

    comments added, files renamed

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1, 10); // 1.2.3.....9
+System.Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+System.Console.WriteLine(numberB);
+int result = numberA + numberB;
+System.Console.WriteLine(result);
---
+++ Example011_VOID_func/Program.cs
@@ -0,0 +1,52 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10);   // наполняет массив рандомными числами. VOID функция, которая не возвращает какой-то результат, просто работает 
+    index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;         // выводит на экран получившийся массив 
+    int position = 0;
+    while(position < count)
+    {
+        System.Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;  // сделано для того, чтобы если заданного числа нет в массиве , возвращался результат -1
+
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index ++;
+    }
+    return position;
+
+}
+
+int[] array = new int [10];     // определили, что нам нужен массив из 10 элементов
+
+FillArray(array);   // заполнили массив 
+array[4] = 4;
+array[6] = 4;   // чтобы в рандомный набор цифир вписать нужную нам 
+
+PrintArray(array);   //  выврдим созданный массив 
+System.Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+System.Console.WriteLine(pos);
